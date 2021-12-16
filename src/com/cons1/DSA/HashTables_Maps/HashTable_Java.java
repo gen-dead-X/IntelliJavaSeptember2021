5956e2d3405d1f99b141c28ff134ea8d5cfcfe08
Joyrudra Biswas <joyrudrabws07@gmail.com>
2021-12-17 00:22:14 +0530

    updated with leet code merged sorted arrays

---
+++ src/com/cons1/DSA/HashTables_Maps/HashTable_Java.java
@@ -0,0 +1,25 @@
+package com.cons1.DSA.HashTables_Maps;
+
+import java.util.*;
+import java.io.*;
+
+public class HashTable_Java {
+    public static void main(String[] args) {
+//        Hashtable<String , String> hashtable = new Hashtable<>();
+        Map<String, String> hashtable = new HashMap<>();
+        hashtable.put("one. ", "Hey! ");
+        hashtable.put("two. ", "This is ");
+        hashtable.put("three. ", "Joy!");
+
+        System.out.println(hashtable.get("one. "));
+//        Set<String> keys = new hashtable.keySet();
+//        Set<Map.Entry<String, String>> value = hashtable.entrySet();
+        for( String key : hashtable.keySet()) {
+            System.out.println(hashtable.get(key));
+        }
+
+
+
+
+    }
+}
---
+++ src/com/cons1/LeetCode/MergedSortedArrays.java
@@ -0,0 +1,29 @@
+package com.cons1.LeetCode;
+
+public class MergedSortedArrays {
+    public static void main(String[] args) {
+        int arr1[] = {0, 1, 3, 5, 7};
+        int arr2[] = {2, 4, 5, 6, 8};
+
+        int arr[] = new int[arr1.length + arr2.length];
+        int pos1 = 0, pos2 = 0;
+
+        for(int i = 0; i < arr.length - 1 ; i++){
+            if(arr1[pos1] < arr2[pos2]){
+                arr[i] = arr1[pos1++];
+            }
+            else if(arr1[pos1] > arr2[pos2]){
+                arr[i] = arr2[pos2++];
+            }
+            else{
+                arr[i++] = arr1[pos1++];
+                arr[i] = arr2[pos2++];
+            }
+
+        }
+
+        for(int i = 0 ; i < arr.length ; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
