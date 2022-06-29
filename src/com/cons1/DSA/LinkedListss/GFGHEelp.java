ac03ba56b721dbc6e8da94954b0dad1490e4565e
Joyrudra Biswas <joyrudrabws07@gmail.com>
2022-06-29 12:54:29 +0530

    Update with DSA

---
+++ src/com/cons1/DSA/LinkedListss/GFGHEelp.java
@@ -0,0 +1,31 @@
+package com.cons1.DSA.LinkedListss;
+
+import java.util.LinkedList;
+
+public class GFGHEelp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Linked List : ");
+
+        LinkedList<String> list = new LinkedList();
+
+        list.add("Sobhabazar Sutanoti");
+        list.add("EsPlanade");
+        list.add("Kabi Subhas");
+        list.add("Belgachia");
+        list.add("EsPlanade");
+        list.add("Mastar Da Surjo Sen");
+
+
+
+        list.add(4,"Kalighat");
+
+        list.removeFirst();
+        list.removeLast();
+        list.removeFirstOccurrence("EsPlanade");
+
+
+//        for(list.size())
+        System.out.println(list);
+    }
+
+}
